563eb7d13991757c8284564e438a463d46be64d3
Tmk <tomasz.kajda@solarwinds.com>
2022-01-14 11:32:15 +0100

    Initial changes

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ObseravabilityEasyDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var overrideDlls = true;
+            var pdbs = System.IO.Directory.GetFiles(args[0], "*.pdb");
+            var dlls = System.IO.Directory.GetFiles(args[1], "*.dll", SearchOption.AllDirectories);
+            foreach (var filePath in pdbs)
+            {
+                var test = Regex.Match(filePath, @".*\\([^\\]+$)").Groups;
+                string pdbName = Regex.Match(filePath, @".*\\([^\\]+$)").Groups[1].Value;
+                string filename = pdbName.Replace(".pdb",".dll");
+                var destinationDir = dlls.Where(x => x.Contains(filename)).Select(p => Regex.Match(p, @".*\\([^\\]+$)").Groups[0])
+                    .FirstOrDefault();
+                var destincationDir = Path.GetDirectoryName(destinationDir.ToString());
+                var destPath = $"{destincationDir}\\{pdbName}";
+                var destDllPath = $"{destincationDir}\\{filename}";
+                File.Copy(filePath, destPath, true);
+                if (overrideDlls)
+                {
+                    try
+                    {
+                        File.Copy(filePath.Replace(".pdb", ".dll"), destDllPath, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"[ERROR] : {ex.Message}");
+                    }
+                }
+                Console.WriteLine($"[OK] {filePath}>{destincationDir}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # observabilityEasyDebug
+The application getting pointed PDB files and search matching dlls in destination directory, if find matches trying to copy *.PDB to the dll's location.
+
+The first parameter is a location of *.PDB files
+The second parameter is location of root OrionDirectory on loacl or remote machine (if remote write permisions needed)
+Example arguments:
+C:\git\orion\src\web\src\SolarWinds.IPAM\SolarWinds.IPAM.Web.Common\bin\Debug\net48 \\TKAJD-R01\Orion
