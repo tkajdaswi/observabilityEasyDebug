fb6bfb0dc9f3a28728797b748083cf50576ce372
Tmk <tomasz.kajda@solarwinds.com>
2022-01-26 09:33:24 +0100

    program changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -9,35 +10,54 @@ namespace ObseravabilityEasyDebug
     {
         static void Main(string[] args)
         {
-            var overrideDlls = true;
-            var pdbs = System.IO.Directory.GetFiles(args[0], "*.pdb");
-            var dlls = System.IO.Directory.GetFiles(args[1], "*.dll", SearchOption.AllDirectories);
-            foreach (var filePath in pdbs)
+            try
             {
-                var test = Regex.Match(filePath, @".*\\([^\\]+$)").Groups;
-                string pdbName = Regex.Match(filePath, @".*\\([^\\]+$)").Groups[1].Value;
-                string filename = pdbName.Replace(".pdb",".dll");
-                var destinationDir = dlls.Where(x => x.Contains(filename)).Select(p => Regex.Match(p, @".*\\([^\\]+$)").Groups[0])
-                    .FirstOrDefault();
-                var destincationDir = Path.GetDirectoryName(destinationDir.ToString());
-                var destPath = $"{destincationDir}\\{pdbName}";
-                var destDllPath = $"{destincationDir}\\{filename}";
-                File.Copy(filePath, destPath, true);
-                if (overrideDlls)
+                var overrideDlls = true;
+                var pdbs = System.IO.Directory.GetFiles(args[0], "*.pdb");
+                var destinationPaths = args.Skip(1);
+                var dlls = new List<string>();
+                foreach (var path in destinationPaths)
                 {
-                    try
-                    {
-                        File.Copy(filePath.Replace(".pdb", ".dll"), destDllPath, true);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"[ERROR] : {ex.Message}");
-                    }
+                    dlls.AddRange(Directory.GetFiles(args[1], "*.dll", SearchOption.AllDirectories).ToList());
                 }
-                Console.WriteLine($"[OK] {filePath}>{destincationDir}");
-            }
+                foreach (var filePath in pdbs)
+                {
+                    var test = Regex.Match(filePath, @".*\\([^\\]+$)").Groups;
+                    string pdbName = Regex.Match(filePath, @".*\\([^\\]+$)").Groups[1].Value;
+                    string filename = pdbName.Replace(".pdb", ".dll");
+                    var destinationDir = dlls.Where(x => x.Contains(filename))
+                        .Select(p => Regex.Match(p, @".*\\([^\\]+$)").Groups[0])
+                        .ToList();
+                    foreach (var dir in destinationDir)
+                    {
+                        var destincationDir = Path.GetDirectoryName(dir.ToString());
+                        var destPath = $"{destincationDir}\\{pdbName}";
+                        var destDllPath = $"{destincationDir}\\{filename}";
+                        File.Copy(filePath, destPath, true);
+                        if (overrideDlls)
+                        {
+                            try
+                            {
+                                File.Copy(filePath.Replace(".pdb", ".dll"), destDllPath, true);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"[ERROR] : {ex.Message}");
+                            }
+                        }
 
-            Console.ReadLine();
+                        Console.WriteLine($"[OK] {filePath}>{destincationDir}");
+                    }
+                    
+
+                }
+
+                Console.WriteLine("FINISHED");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
